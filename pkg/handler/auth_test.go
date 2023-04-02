8393bda1604e6fefe7febd248be5b98e06383f70
Ivan <fistashkin0123@gmail.com>
2023-04-02 21:32:57 +0300

    Covered code with tests

--- pkg/handler/auth_test.go
+++ pkg/handler/auth_test.go
@@ -1,1 +1,82 @@
 package handler
+
+import (
+	"bytes"
+	"github.com/gavrylenkoIvan/todo-app-gin"
+	"github.com/gavrylenkoIvan/todo-app-gin/pkg/service"
+	mock_service "github.com/gavrylenkoIvan/todo-app-gin/pkg/service/mocks"
+	"github.com/gin-gonic/gin"
+	"github.com/golang/mock/gomock"
+	"github.com/magiconair/properties/assert"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestHandler_signUp(t *testing.T) {
+	type mockBehavior func(s *mock_service.MockAuthorization, user todo.User)
+
+	testTable := []struct {
+		name                string
+		inputBody           string
+		inputUser           todo.User
+		mockBehavior        mockBehavior
+		expectedStatusCode  int
+		expectedRequestBody string
+	}{
+		{
+			name:      "OK",
+			inputBody: `{"name": "Test","username":"test","password":"qwerty"}`,
+			inputUser: todo.User{
+				Name:     "Test",
+				Username: "test",
+				Password: "qwerty",
+			},
+			mockBehavior: func(s *mock_service.MockAuthorization, user todo.User) {
+				s.EXPECT().CreateUser(user).Return(1, nil)
+			},
+			expectedStatusCode:  200,
+			expectedRequestBody: `{"id":1}`,
+		},
+		{
+			name:      "Invalid input body",
+			inputBody: `{"username":"test","password":"qwerty"}`,
+			inputUser: todo.User{
+				Name:     "Test",
+				Username: "test",
+				Password: "qwerty",
+			},
+			mockBehavior:        func(s *mock_service.MockAuthorization, user todo.User) {},
+			expectedStatusCode:  400,
+			expectedRequestBody: `{"message":"invalid input body"}`,
+		},
+	}
+
+	for _, testCase := range testTable {
+		t.Run(testCase.name, func(t *testing.T) {
+			c := gomock.NewController(t)
+			defer c.Finish()
+
+			auth := mock_service.NewMockAuthorization(c)
+			testCase.mockBehavior(auth, testCase.inputUser)
+
+			services := &service.Service{Authorization: auth}
+			handler := NewHandler(services)
+
+			// Test server
+			r := gin.New()
+			r.POST("/sign-up", handler.signUp)
+
+			// Test request
+			w := httptest.NewRecorder()
+			req := httptest.NewRequest("POST", "/sign-up",
+				bytes.NewBufferString(testCase.inputBody))
+
+			// Perform request
+			r.ServeHTTP(w, req)
+
+			// Assert
+			assert.Equal(t, testCase.expectedStatusCode, w.Code)
+			assert.Equal(t, testCase.expectedRequestBody, w.Body.String())
+		})
+	}
+}
---
+++ pkg/handler/middleware_test.go
@@ -0,0 +1,87 @@
+package handler
+
+import (
+	"fmt"
+	"github.com/gavrylenkoIvan/todo-app-gin/pkg/service"
+	mock_service "github.com/gavrylenkoIvan/todo-app-gin/pkg/service/mocks"
+	"github.com/gin-gonic/gin"
+	"github.com/golang/mock/gomock"
+	"github.com/magiconair/properties/assert"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestHandler_userIdentity(t *testing.T) {
+	type mockBehavior func(s *mock_service.MockAuthorization, token string)
+
+	testTable := []struct {
+		name                 string
+		headerName           string
+		headerValue          string
+		token                string
+		mockBehavior         mockBehavior
+		expectedStatusCode   int
+		expectedResponseBody string
+	}{
+		{
+			name:        "OK",
+			headerName:  "Authorization",
+			headerValue: "Bearer token",
+			token:       "token",
+			mockBehavior: func(s *mock_service.MockAuthorization, token string) {
+				s.EXPECT().ParseToken(token).Return(1, nil)
+			},
+			expectedStatusCode:   200,
+			expectedResponseBody: "1",
+		},
+		{
+			name:                 "Empty header",
+			headerName:           "",
+			mockBehavior:         func(s *mock_service.MockAuthorization, token string) {},
+			expectedStatusCode:   401,
+			expectedResponseBody: `{"message":"empty auth header"}`,
+		},
+		{
+			name:                 "Invalid header",
+			headerName:           "Authorization",
+			headerValue:          "Bearr token",
+			token:                "token",
+			mockBehavior:         func(s *mock_service.MockAuthorization, token string) {},
+			expectedStatusCode:   401,
+			expectedResponseBody: `{"message":"invalid auth header"}`,
+		},
+	}
+
+	for _, testCase := range testTable {
+		t.Run(testCase.name, func(t *testing.T) {
+			// Init deps
+			c := gomock.NewController(t)
+			defer c.Finish()
+
+			auth := mock_service.NewMockAuthorization(c)
+			testCase.mockBehavior(auth, testCase.token)
+
+			services := &service.Service{Authorization: auth}
+			handler := NewHandler(services)
+
+			// Test server
+			r := gin.New()
+			r.GET("/protected", handler.userIdentity, func(context *gin.Context) {
+				id, _ := context.Get(userCtx)
+				context.String(200, fmt.Sprintf("%d", id.(int)))
+			})
+
+			// Test request
+			w := httptest.NewRecorder()
+			req := httptest.NewRequest("GET", "/protected", nil)
+			req.Header.Set(testCase.headerName, testCase.headerValue)
+
+			// Make request
+			r.ServeHTTP(w, req)
+
+			// Assert
+			assert.Equal(t, w.Code, testCase.expectedStatusCode)
+			assert.Equal(t, w.Body.String(), testCase.expectedResponseBody)
+		})
+	}
+}
